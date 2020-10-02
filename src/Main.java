0c91fef9aa641d445dabaa7acbdb8fa74a662f53
Rakotonirina Mano <manoarison@gmail.com>
2020-10-02 09:21:32 +0200

    Main : test toString() method from Fraction class

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args ) {
         System.out.println("Hello World !");
+        Fraction f =  new Fraction(1,2);
+        String test = f.toString();
+        System.out.println(test);
     }
 }
