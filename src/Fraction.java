62f4520db05c222e9efb5265c4db5fa7deb73f27
Rakotonirina Mano <manoarison@gmail.com>
2020-10-02 09:21:57 +0200

    Fraction : creates a fraction with num and denum in arg

--- src/Fraction.java
+++ src/Fraction.java
@@ -1,3 +1,13 @@
 public class Fraction {
+    private int num;
+    private int denom;
 
+    public Fraction(int num,int denom){
+        this.num = num;
+        this.denom = denom;
+    }
+
+    public String toString(){
+        return "Numerateur = "+num+ " / Denominateur = "+denom ;
+    }
 }
